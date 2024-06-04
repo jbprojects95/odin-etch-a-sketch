9510738510b9226a959cc4d46eebfa1de91c2ab3
jbprojects95 <158042318+jbprojects95@users.noreply.github.com>
2024-06-04 10:12:33 +0100

    Added slider for grid control, changed layout

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-function makeDivs(numDivs) {
+// function makeDivs(numDivs) {
+//   const container = document.querySelector(".grid-container");
+
+//   for (let d = 0; d < numDivs * numDivs; d++) {
+//     let cells = document.createElement("div");
+//     const boxSize = 450 / numDivs;
+//     cells.classList.add("cells");
+//     cells.style.width = boxSize + "px";
+//     cells.style.height = boxSize + "px";
+//     container.appendChild(cells);
+//   }
+// }
+
+// makeDivs(16);
+
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".range-value");
+slider.addEventListener("change", () => {
   const container = document.querySelector(".grid-container");
-  for (let d = 0; d < numDivs * numDivs; d++) {
+  sliderValue.textContent = slider.value;
+  if (container.hasChildNodes()) {
+    container.innerHTML = "";
+  }
+  makeDivs(slider.value);
+});
+
+const makeDivs = (numValue) => {
+  const container = document.querySelector(".grid-container");
+  for (let d = 0; d < numValue * numValue; d++) {
     let cells = document.createElement("div");
-    const boxSize = 450 / numDivs;
+    const boxSize = 500 / numValue;
     cells.classList.add("cells");
     cells.style.width = boxSize + "px";
     cells.style.height = boxSize + "px";
     container.appendChild(cells);
   }
-}
+};
 
 makeDivs(16);
 
@@ -24,7 +50,10 @@ let clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", () => {
   const gridDivs = document.querySelectorAll(".cells");
   gridDivs.forEach((div) => {
-    div.classList.remove("colour-change");
+    div.style.backgroundColor = "white";
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
   });
 });
 
@@ -34,7 +63,18 @@ eraseBtn.addEventListener("click", () => {
   const hoverErase = document.querySelectorAll(".cells");
   hoverErase.forEach((div) => {
     div.addEventListener("mouseover", function () {
-      div.classList.remove("colour-change");
+      div.style.backgroundColor = "white";
+    });
+  });
+});
+
+drawBtn = document.querySelector(".draw");
+
+drawBtn.addEventListener("click", () => {
+  const blackHover = document.querySelectorAll(".cells");
+  blackHover.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
     });
   });
 });
